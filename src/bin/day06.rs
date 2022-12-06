03e9fb79718e454a2f618b3f1cfd2940ff2325a3
Íñigo Huguet <inigohuguet@hotmail.com>
2022-12-06 17:44:28 +0100

    refactor day 6

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,23 +1,18 @@
-use std::fs;
-use std::collections::BTreeSet;
+use std::{fs, collections::BTreeSet};
 
 fn main() {
 	let signal = fs::read_to_string("input/day06.txt").unwrap();
 	let signal = signal.trim().as_bytes();
+	println!("Part 1: start-of-message at pos {}", find_unique_serie(&signal, 4).unwrap());
+	println!("Part 2: start-of-packet at pos {}", find_unique_serie(&signal, 14).unwrap());
+}
 
-	for i in 0..signal.len() {
-		let chars: BTreeSet<u8> = signal[i..i+4].iter().copied().collect();
-		if chars.len() == 4 {
-			println!("Part 1: start-of-message at pos {}", i + 4);
-			break;
+fn find_unique_serie(signal: &[u8], len: usize) -> Option<usize> {
+	for i in 0..signal.len() - len{
+		let chars: BTreeSet<u8> = signal[i..i + len].iter().copied().collect();
+		if chars.len() == len {
+			return Some(i + len);
 		}
 	}
-
-	for i in 0..signal.len() {
-		let chars: BTreeSet<u8> = signal[i..i+14].iter().copied().collect();
-		if chars.len() == 14 {
-			println!("Part 2: start-of-packet at pos {}", i + 14);
-			break;
-		}
-	}
+	None
 }
