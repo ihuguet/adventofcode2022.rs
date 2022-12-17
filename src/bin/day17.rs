8a2ce8b730e4a829eba02132432df6cfd3e60d8f
Íñigo Huguet <inigohuguet@hotmail.com>
2022-12-18 00:44:29 +0100

    refactor day 17

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -10,115 +10,139 @@ const DOWN: Point<isize> = Point {y: -1, x: 0};
 #[derive(PartialEq)]
 struct Shape(Vec<Vec<bool>>);
 
+type State = (usize, Vec<bool>);
+
+struct Tower {
+	grid: VecGrid<bool>,
+	height: usize,
+	height_sum: usize,
+	rocks_count: usize,
+	states: HashMap<State, (usize, usize)>
+}
+
 fn main() {
 	let jet_pattern = parse_input();
 	let shapes = create_shapes();
-	println!("Part 1: tower {} high", solve(&jet_pattern, &shapes, 2022));
-	println!("Part 2: tower {} high", solve(&jet_pattern, &shapes, 1000000000000));
+	println!("Part 1: tower {} high", Tower::solve(&jet_pattern, &shapes, 2022));
+	println!("Part 2: tower {} high", Tower::solve(&jet_pattern, &shapes, 1000000000000));
 }
 
-fn solve(jet_pattern: &str, shapes: &[Shape], limit: usize) -> usize {
-	let mut tower = vec![vec![false; 7]; 4000];
-
-	let mut jet_pattern_iter = jet_pattern.chars().to_owned().cycle();
-	let mut shapes_iter = (0..shapes.len()).cycle().skip(1);
-
-	let mut stopped_shapes = 0;
-	let mut tower_height = 0;
-	let mut shape = &shapes[0];
-	let mut shape_n;
-	let mut pos = Point::from((3usize, 2usize));
-
-	let mut states = HashMap::new();
-	let mut tower_height_sum = 0;
-
-	while stopped_shapes < limit {
-
-		let mov = jet_pattern_iter.next().unwrap();
-		let mov_result = match mov {
-			'<' => check_move(&tower, shape, pos, LEFT),
-			'>' => check_move(&tower, shape, pos, RIGHT),
-			ch => panic!("Unexpected '{}'", ch)
-		};
-		if let Ok(mov) = mov_result {
-			pos = pos.add_signed(mov);
+impl Tower {
+	fn new() -> Self {
+		Tower {
+			grid: VecGrid::new(),
+			height: 0,
+			height_sum: 0,
+			rocks_count: 0,
+			states: HashMap::new()
 		}
+	}
 
-		if let Ok(mov) = check_move(&tower, shape, pos, DOWN) {
-			pos = pos.add_signed(mov);
-		} else {
-			place_shape(&mut tower, shape, pos);
-			tower_height = tower_height.max(pos.y + shape.height());
-			stopped_shapes += 1;
+	fn solve(jet_pattern: &[Point<isize>], shapes: &[Shape], limit: usize) -> usize {
+		let mut tower = Tower::new();
+		let mut jets_iter = jet_pattern.iter().copied().cycle();
+		let mut shapes_iter = shapes.into_iter().enumerate().cycle();
 
-			shape_n = shapes_iter.next().unwrap();
-			shape = &shapes[shape_n];
-			pos = Point::from((tower_height + 3, 2));
+		let (mut shape_n, mut shape) = shapes_iter.next().unwrap();
+		let mut pos = tower.prepare_next_rock_and_get_pos();
 
-			// fast advance?
-			if let Some(state) = get_state(&tower, tower_height, shape_n) {
-				if states.contains_key(&state) {
-					let (prev_shapes, prev_height) = states[&state];
-					let left = limit - stopped_shapes;
-					let times = left / (stopped_shapes - prev_shapes);
-					stopped_shapes += (stopped_shapes - prev_shapes) * times;
-					tower_height_sum = (tower_height - prev_height) * times;
-					states.clear();
-				}
-
-				states.insert(state, (stopped_shapes, tower_height));
+		while tower.rocks_count < limit {
+			// left/right
+			let mov = jets_iter.next().unwrap();
+			if let Ok(mov) = tower.check_move(shape, pos, mov) {
+				pos = pos.add_signed(mov);
 			}
 
-			grow_tower_for_height(&mut tower, pos.y + shape.height());
+			// down
+			if let Ok(mov) = tower.check_move(shape, pos, DOWN) {
+				pos = pos.add_signed(mov);
+			} else {
+				tower.place_shape(shape, pos);
+				tower.maybe_fast_advance(limit, shape_n);
+
+				(shape_n, shape) = shapes_iter.next().unwrap();
+				pos = tower.prepare_next_rock_and_get_pos();
+			}
 		}
+
+		tower.height + tower.height_sum
 	}
 
-	tower_height + tower_height_sum
-}
+	fn prepare_next_rock_and_get_pos(&mut self) -> Point {
+		let pos = Point::from((self.height + 3, 2));
 
-fn check_move(tower: &VecGrid<bool>, shape: &Shape, pos: Point, mov: Point<isize>) -> Result<Point<isize>, ()> {
-	let pos_new: Point<isize> = Point::from(pos) + mov;
+		while self.grid.len() < pos.y + 4 {
+			self.grid.push(vec![false; TOWER_WIDTH]);
+		}
 
-	if pos_new.y < 0 || pos_new.x < 0 {
-		return Err(());
+		pos
 	}
 
-	let pos_new: Point = pos_new.into();
+	fn check_move(&self, shape: &Shape, pos: Point, mov: Point<isize>) -> Result<Point<isize>, ()> {
+		let pos_new: Point<isize> = Point::from(pos) + mov;
 
-	for shape_point in shape.iter() {
-		let point = pos_new + shape_point;
-		if point.x >= TOWER_WIDTH || tower[point] == true {
+		if pos_new.y < 0 || pos_new.x < 0 {
 			return Err(());
 		}
+
+		let pos_new: Point = pos_new.into();
+
+		for shape_point in shape.iter() {
+			let point = pos_new + shape_point;
+			if point.x >= TOWER_WIDTH || self.grid[point] == true {
+				return Err(());
+			}
+		}
+
+		Ok(mov)
 	}
 
-	Ok(mov)
-}
+	fn place_shape(&mut self, shape: &Shape, pos: Point) {
+		for shape_point in shape.iter() {
+			self.grid[pos + shape_point] = true;
+		}
 
-fn place_shape(tower: &mut VecGrid<bool>, shape: &Shape, pos: Point) {
-	for shape_point in shape.iter() {
-		tower[pos + shape_point] = true;
+		self.height = self.height.max(pos.y + shape.height());
+		self.rocks_count += 1;
 	}
-}
 
-fn grow_tower_for_height(tower: &mut VecGrid<bool>, height: usize) {
-	if tower.len() < height {
-		tower.reserve(tower.len());
-		tower.extend(std::iter::repeat(vec![false; 7]).take(tower.len()));
+	fn maybe_fast_advance(&mut self, rocks_limit: usize, shape_n: usize) {
+		if let Some(state) = self.get_state(shape_n) {
+			if self.states.contains_key(&state) {
+				let (prev_rocks, prev_height) = self.states[&state];
+				let left = rocks_limit - self.rocks_count;
+				let times = left / (self.rocks_count - prev_rocks);
+				self.rocks_count += (self.rocks_count - prev_rocks) * times;
+				self.height_sum = (self.height - prev_height) * times;
+				self.states.clear();
+			}
+
+			self.states.insert(state, (self.rocks_count, self.height));
+		}
+
 	}
-}
 
-fn get_state(tower: &VecGrid<bool>, height: usize, shape_n: usize) -> Option<(usize, Vec<bool>)> {
-	if height >= 100 {
-		let top: Vec<bool> = tower[height - 100..height].iter().flatten().map(|v| *v).collect();
-		Some((shape_n, top))
-	} else {
-		None
+	fn get_state(&self, shape_n: usize) -> Option<State> {
+		if self.height >= 100 {
+			let top: Vec<bool> = self.grid[self.height - 100..self.height].iter()
+				.flatten()
+				.map(|v| *v)
+				.collect();
+
+			Some((shape_n, top))
+		} else {
+			None
+		}
 	}
-}
+} // impl Tower
 
-fn parse_input() -> String{
-	aoc::input::read_lines("day17").next().unwrap()
+fn parse_input() -> Vec<Point<isize>> {
+	let line = aoc::input::read_lines("day17").next().unwrap();
+	line.chars().map(|ch| match ch {
+		'<' => LEFT,
+		'>' => RIGHT,
+		_ => panic!()
+	}).collect()
 }
 
 fn create_shapes() -> Vec<Shape> {
@@ -126,26 +150,22 @@ fn create_shapes() -> Vec<Shape> {
 		Shape(vec![
 			vec![true, true, true, true]
 		]),
-
 		Shape(vec![
 			vec![false, true, false],
 			vec![true, true, true],
 			vec![false, true, false]
 		]),
-
 		Shape(vec![
 			vec![true, true, true],
 			vec![false, false, true],
 			vec![false, false, true]
 		]),
-
 		Shape(vec![
 			vec![true],
 			vec![true],
 			vec![true],
 			vec![true]
 		]),
-
 		Shape(vec![
 			vec![true, true],
 			vec![true, true]
