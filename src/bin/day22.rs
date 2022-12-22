b41af7db408363cbbb68085c20fc311acc61ac75
Íñigo Huguet <inigohuguet@hotmail.com>
2022-12-22 15:33:45 +0100

    refactor day 22

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -4,9 +4,9 @@ use std::collections::BTreeMap;
 use Orientation::*;
 
 type Point = aoc::space_2D::Point<isize>;
-type WrapFn = dyn Fn(&VecGrid<u8>, Point, Orientation) -> (Point, Orientation);
+type WrapFn = dyn Fn(&VecGrid<u8>, Traveler) -> Traveler;
 
-#[derive(Clone)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Traveler {
 	pos: Point,
 	orientation: Orientation
@@ -28,8 +28,8 @@ fn main() {
 	println!("Part1: result {}", solve(&map, dirs.clone(), &wrap));
 
 	let mappings = create_cube_wrap();
-	let wrap_cube = move |_m: &Vec<Vec<u8>>, pos: Point, orientation: Orientation| -> (Point, Orientation) {
-		mappings[&(pos, orientation)]
+	let wrap_cube = move |_m: &Vec<Vec<u8>>, traveler: Traveler| -> Traveler {
+		mappings[&traveler]
 	};
 	println!("Part2: result {}", solve(&map, dirs, &wrap_cube));
 }
@@ -39,14 +39,13 @@ fn solve(map: &VecGrid<u8>, mut indications: Indications, wrap_fn: &WrapFn) -> i
 	let mut traveler = Traveler {pos: (0, col_start).into(), orientation: RIGHT};
 
 	while !indications.done() {
-		if let Some(mut steps) = indications.get_next_move_steps() {
-			while steps > 0 {
+		if let Some(steps) = indications.get_next_move_steps() {
+			for _ in 0..steps {
 				let next = move_maybe_wrap(map, &traveler, wrap_fn);
 				if map[next.pos] == b'#' {
 					break;
 				}
 				traveler = next;
-				steps -= 1;
 			}
 		}
 
@@ -59,14 +58,14 @@ fn solve(map: &VecGrid<u8>, mut indications: Indications, wrap_fn: &WrapFn) -> i
 }
 
 fn move_maybe_wrap(map: &VecGrid<u8>, traveler: &Traveler, wrap_fn: &WrapFn) -> Traveler {
-	let mut pos = traveler.pos + traveler.orientation.get_move_dir();
-	let mut orientation = traveler.orientation;
+	let mut traveler = traveler.clone();
+	traveler.pos += traveler.orientation.get_move_dir();
 
-	if map.get_point(pos).is_none() || map[pos] == b' ' {
-		(pos, orientation) = wrap_fn(&map, pos, orientation);
+	if map.get_point(traveler.pos).is_none() || map[traveler.pos] == b' ' {
+		traveler = wrap_fn(&map, traveler);
 	}
 
-	Traveler {pos, orientation}
+	traveler
 }
 
 impl Indications {
@@ -123,41 +122,43 @@ impl Orientation {
 	}
 }
 
-fn wrap(map: &VecGrid<u8>, pos: Point, orientation: Orientation) -> (Point, Orientation) {
-	let (y, x) = (pos.y as usize, pos.x as usize);
-	let pos = match orientation {
+fn wrap(map: &VecGrid<u8>, mut traveler: Traveler) -> Traveler {
+	let (y, x) = (traveler.pos.y as usize, traveler.pos.x as usize);
+	let pos = match traveler.orientation {
 		RIGHT => (y, map[y].iter().position(|ch| *ch != b' ').unwrap()),
 		DOWN  => (map.iter().position(|row| row[x] != b' ').unwrap(), x),
 		LEFT  => (y, map[y].iter().rposition(|ch| *ch != b' ').unwrap()),
 		UP    => (map.iter().rposition(|row| row[x] != b' ').unwrap(), x)
 	};
-	(Point::from((pos.0 as isize, pos.1 as isize)), orientation)
+	traveler.pos = (pos.0 as isize, pos.1 as isize).into();
+	traveler
 }
 
-fn create_cube_wrap() -> BTreeMap<(Point, Orientation), (Point, Orientation)> {
-	let mut mappings: BTreeMap<(Point, Orientation), (Point, Orientation)> = BTreeMap::new();
+fn create_cube_wrap() -> BTreeMap<Traveler, Traveler> {
+	let mut mappings: BTreeMap<Traveler, Traveler> = BTreeMap::new();
+	let tr = |y, x, orientation| Traveler {pos: Point::from((y, x)), orientation};
 
 	for n in 0..50 {
-		mappings.insert(((n, 49).into(), LEFT), ((149 - n, 0).into(), RIGHT));
-		mappings.insert(((149 - n, -1).into(), LEFT), ((n, 50).into(), RIGHT));
+		mappings.insert(tr(n, 49, LEFT), tr(149 - n, 0, RIGHT));
+		mappings.insert(tr(149 - n, -1, LEFT), tr(n, 50, RIGHT));
 
-		mappings.insert(((-1, 50 + n).into(), UP), ((150 + n, 0).into(), RIGHT));
-		mappings.insert(((150 + n, -1).into(), LEFT), ((0, 50 + n).into(), DOWN));
+		mappings.insert(tr(-1, 50 + n, UP), tr(150 + n, 0, RIGHT));
+		mappings.insert(tr(150 + n, -1, LEFT), tr(0, 50 + n, DOWN));
 		
-		mappings.insert(((-1, 100 + n).into(), UP), ((199, n).into(), UP));
-		mappings.insert(((200, n).into(), DOWN), ((0, 100 + n).into(), DOWN));
+		mappings.insert(tr(-1, 100 + n, UP), tr(199, n, UP));
+		mappings.insert(tr(200, n, DOWN), tr(0, 100 + n, DOWN));
 
-		mappings.insert(((n, 150).into(), RIGHT), ((149 - n, 99).into(), LEFT));
-		mappings.insert(((149 - n, 100).into(), RIGHT), ((n, 149).into(), LEFT));
+		mappings.insert(tr(n, 150, RIGHT), tr(149 - n, 99, LEFT));
+		mappings.insert(tr(149 - n, 100, RIGHT), tr(n, 149, LEFT));
 
-		mappings.insert(((50, 100 + n).into(), DOWN), ((50 + n, 99).into(), LEFT));
-		mappings.insert(((50 + n, 100).into(), RIGHT), ((49, 100 + n).into(), UP));
+		mappings.insert(tr(50, 100 + n, DOWN), tr(50 + n, 99, LEFT));
+		mappings.insert(tr(50 + n, 100, RIGHT), tr(49, 100 + n, UP));
 
-		mappings.insert(((50 + n, 49).into(), LEFT), ((100, n).into(), DOWN));
-		mappings.insert(((99, n).into(), UP), ((50 + n, 50).into(), RIGHT));
+		mappings.insert(tr(50 + n, 49, LEFT), tr(100, n, DOWN));
+		mappings.insert(tr(99, n, UP), tr(50 + n, 50, RIGHT));
 
-		mappings.insert(((150, 50 + n).into(), DOWN), ((150 + n, 49).into(), LEFT));
-		mappings.insert(((150 + n, 50).into(), RIGHT), ((149, 50 + n).into(), UP));
+		mappings.insert(tr(150, 50 + n, DOWN), tr(150 + n, 49, LEFT));
+		mappings.insert(tr(150 + n, 50, RIGHT), tr(149, 50 + n, UP));
 	}
 
 	mappings
